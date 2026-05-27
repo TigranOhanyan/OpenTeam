799cfa61586673bf3bd447dce453df3b10fa02ee
tigranohanyan <tigranohanyan777@gmail.com>
2026-05-27 20:45:07 +0400

    feat: make runtime interface

---
+++ runtime.go
@@ -0,0 +1,12 @@
+package openteam
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+type Runtime interface {
+	Ask(ctx context.Context, memberName string, channelName string, message string, logger *zap.Logger) (runIds []string, err error)
+	Run(ctx context.Context, runId string, logger *zap.Logger) (reply Reply, err error)
+}
